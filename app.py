b59b91d4d6a5ea3e89b510fa0b30a2294dffc0dc
Atos Ikonen <atos.ikonen@gmail.com>
2025-09-21 19:10:25 +0300

    modified:   .gitignore 	modified:   app.py 	new file:   config.py 	new file:   db.py 	new file:   forum.py 	new file:   schema.sql 	new file:   templates/edit.html 	new file:   templates/form.html 	new file:   templates/index.html 	new file:   templates/login.html 	new file:   templates/new_thread.html 	new file:   templates/register.html 	new file:   templates/remove.html 	new file:   templates/search.html 	new file:   templates/thread.html 	new file:   users.py

--- app.py
+++ app.py
@@ -1,15 +1,139 @@
+import sqlite3
 from flask import Flask
+from flask import abort
+from flask import redirect, render_template, request, session
+import config, forum, users
+from werkzeug.security import generate_password_hash
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
+
+def require_login():
+    if "user_id" not in session:
+        abort(403)
 
 @app.route("/")
 def index():
-    return "Tervetuloa sovellukseen!"
+    threads = forum.get_threads()
+    return render_template("index.html", threads=threads)
 
-@app.route("/page1")
-def page1():
-    return "Ensimmäinen sivu"
+@app.route("/thread/<int:thread_id>")
+def show_thread(thread_id):
+    thread = forum.get_thread(thread_id)
+    if not thread:
+        abort(404)
+    messages = forum.get_messages(thread_id)
+    return render_template("thread.html", thread=thread, messages=messages)
 
-@app.route("/page2")
-def page2():
-    return "Toinen sivu"
+@app.route("/new_thread", methods=["POST"])
+def new_thread():
+    require_login()
+
+    title = request.form["title"]
+    content = request.form["content"]
+    user_id = session["user_id"]
+    if not title or not content or len(title) > 100 or len(content) > 5000:
+        abort(403)
+
+    thread_id = forum.add_thread(title, content, user_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/new_message", methods=["POST"])
+def new_message():
+    require_login()
+
+    content = request.form["content"]
+    user_id = session["user_id"]
+    thread_id = request.form["thread_id"]
+
+    try:
+        forum.add_message(content, user_id, thread_id)
+    except sqlite3.IntegrityError:
+        abort(403)
+
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/edit/<int:message_id>", methods=["GET", "POST"])
+def edit_message(message_id):
+    message = forum.get_message(message_id)
+    if not message:
+        abort(404)
+    if message["user_id"] != session["user_id"]:
+        abort(403)
+
+    if request.method == "GET":
+        return render_template("edit.html", message=message)
+
+    if request.method == "POST":
+        content = request.form["content"]
+        forum.update_message(message["id"], content)
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/remove/<int:message_id>", methods=["GET", "POST"])
+def remove_message(message_id):
+    message = forum.get_message(message_id)
+    if not message:
+        abort(404)
+    if message["user_id"] != session["user_id"]:
+        abort(403)
+
+    if request.method == "GET":
+        return render_template("remove.html", message=message)
+
+    if request.method == "POST":
+        if "continue" in request.form:
+            forum.remove_message(message["id"])
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return render_template("register.html", error="VIRHE: salasanat eivät täsmää")
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return render_template("register.html", error="VIRHE: tunnus on jo varattu")
+
+    return render_template("register.html", success=True, username=username)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return render_template("login.html", error="VIRHE: väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    del session["user_id"]
+    return redirect("/")
+
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+    results = forum.search(query) if query else []
+    return render_template("search.html", query=query, results=results)
+
+@app.route("/new_thread")
+def post_thread():
+    return render_template("new_thread.html")
